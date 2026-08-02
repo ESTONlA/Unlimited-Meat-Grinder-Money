a43a3116fd8993d13d2de8fd2b3ffcf57966f586
ESTONlA <158593443+ESTONlA@users.noreply.github.com>
2026-08-02 03:21:44 +0300

    Add files via upload

---
+++ UnlimitedGrinderMoney/Core.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using MelonLoader;
+using System.Reflection;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(UnlimitedGrinderMoney.UnlimitedGrinderMoneyMod), "Unlimited meat Grinder Money", "1.0.0", "Estonia")]
+[assembly: MelonGame(null, null)]
+
+namespace UnlimitedGrinderMoney
+{
+    public class UnlimitedGrinderMoneyMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Unlimited Grinder Money loaded.");
+        }
+
+        public override void OnUpdate()
+        {
+            var grinders = UnityEngine.Object.FindObjectsOfType<MeatGrinder>();
+
+            foreach (var grinder in grinders)
+            {
+                if (grinder == null) continue;
+
+                // Make the money reward absurdly high
+                grinder.minMoneyReward = 999999;
+                grinder.maxMoneyReward = 9999999;
+
+                // Optional: make the grinder process much faster
+                grinder.Delay = 0.1f;
+            }
+        }
+    }
+}
